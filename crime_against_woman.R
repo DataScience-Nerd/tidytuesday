fe3f4fceddbd367a13263d356c569f0ef9d6dde0
grtvishnu <optra7@gmail.com>
2020-12-27 00:35:59 +0530

    Update crime_against_woman.R

--- crime_against_woman.R
+++ crime_against_woman.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 crimes_states<- crimes %>% 
         group_by(`STATE/UT`) %>% 
-        summarise(Rape_tot = sum(Rape))
+        summarise(Rape_tot = max(Rape))
 
 crimes %>% 
         count(`STATE/UT`) %>% View()
@@ -19,3 +19,17 @@ crimes_states %>%
         ggplot(aes(reorder(`STATE/UT`, Rape_tot), Rape_tot))+
         geom_bar(stat = "identity")+
         coord_flip()
+
+crimes_states %>% group_by(`STATE/UT`) %>% 
+        summarise(max(Rape_tot))
+
+crimes$crimes_total<- crimes$Rape + crimes$`Dowry Deaths` + crimes$`Kidnapping and Abduction` +crimes$`Assault on women with intent to outrage her modesty` +crimes$`Insult to modesty of Women` + crimes$`Cruelty by Husband or his Relatives` + crimes$`Importation of Girls`
+        
+
+crimes %>% 
+        ggplot(aes(Year, crimes_total ))+
+        geom_bar(stat = "identity", alpha =.5, fill = "red")
+
+
+total<- crimes %>%
+        filter(DISTRICT == "TOTAL")
