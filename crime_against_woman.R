e3fdcae9962dd58e44e318b455891e49c5cc0e38
grtvishnu <optra7@gmail.com>
2020-12-26 23:40:15 +0530

    Create crime_against_woman.R

---
+++ crime_against_woman.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+
+
+crimes_states<- crimes %>% 
+        group_by(`STATE/UT`) %>% 
+        summarise(Rape_tot = sum(Rape))
+
+crimes %>% 
+        count(`STATE/UT`) %>% View()
+
+
+crimes$`STATE/UT`<- toupper(crimes$`STATE/UT`)
+
+crimes_states %>% View()
+crimes$`STATE/UT` <- replace(crimes$`STATE/UT`, crimes$`STATE/UT` == "DELHI UT", "DELHI")
+crimes$`STATE/UT` <- replace(crimes$`STATE/UT`, crimes$`STATE/UT` == "D & N HAVELI", "D&N HAVELI")
+crimes$`STATE/UT` <- replace(crimes$`STATE/UT`, crimes$`STATE/UT` == "A & N ISLANDS", "A&N ISLANDS")
+
+crimes_states %>% 
+        ggplot(aes(reorder(`STATE/UT`, Rape_tot), Rape_tot))+
+        geom_bar(stat = "identity")+
+        coord_flip()
